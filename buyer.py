847696d12d84e9dbb4422db51f59f92cfc4be765
Yana Kryshchuk <kryshchuk@ucu.edu.ua>
2019-05-11 23:29:26 +0300

    added functions

--- buyer.py
+++ buyer.py
@@ -15,36 +15,54 @@ class Buyer(User):
         Initialize a buyer.
         """
         super().__init__(nickname)
-        self.command_list.extend(["find",
-                                  "comparate"])
+        self.commands.update({"best products":"Show you the bestselling " \
+            "products.", \
+            "best sellers":"Gives you list of best sellers with their rating.",\
+            "worst sellers":"Gives you the list of worst sellers.",
+            "best shipping":"Shows you the market with chosen product, where " \
+            "the speed of shipping is the highest.",
+            "alternative":"Gives you options for buying alternative products."})
 
 
     def get_best_sellers(self, category="All"):
         """
         Find Best Sellers in chosen category according to their ratings.
-        It gives our buyer the opportunity to find best options for buying products.
+        It gives our buyer the opportunity to find best options for buying
+        products.
         """
         best_sellers = []
         for product in self.get_best_products(category):
             best_sellers.append(product.seller_name)
         return best_sellers
 
+    def get_worst_sellers(self, category = "ALL"):
+        """
+        Find Best Sellers in chosen category according to their ratings.
+        It gives our buyer the opportunity to avoid bad options for buying
+        products.
+        """
+        worst_sellers = []
 
-    def get_id(self):
+    def get_alternative(self, product_name):
         """
-        Return the id of the buyer.
+        Return the list of alternative products.
         """
+        products = search_products(product_name)
+        alternatives = []
+        for product in products:
+            for i in range(len(products) - 1):
+                for j in range(len(products) - 2):
+                    if products[i].product_id == products[j].product_id:
+                        alternatives.append(products[i])
+        return alternatives
 
     def get_ratings(self):
         """
-        Return the instance of BuyerRatings class, which contains all details about
-        the ratings.
+        Return the instance of BuyerRatings class, which contains all details
+        about the ratings.
         """
 
-    def get_order_history(self):
-        """
-        Return the list of shops where the buyer made orders.
-        """
+
 
 
 if __name__ == '__main__':

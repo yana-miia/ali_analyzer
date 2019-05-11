49c7c51177e5ba1c61bc9f7ba4e176c0f11ff715
Yana Kryshchuk <kryshchuk@ucu.edu.ua>
2019-05-11 23:40:44 +0300

    uncommented

--- main.py
+++ main.py
@@ -75,8 +75,6 @@ if usertype == 'Seller':
             print("You called wrong command.")
             print()
 
-
-
 elif usertype == 'Buyer':
 
     buyer = Buyer(nick)
@@ -95,28 +93,27 @@ elif usertype == 'Buyer':
         if command == 'stop':
             break
 
-        # elif command == 'best products':
-        #     print(categories, '\n')
-        #     ctgr = input("Choose category from given list: ")
-        #     pprint(buyer.get_best_products(ctgr))
-        #
+        elif command == 'best products':
+            print(categories, '\n')
+            ctgr = input("Choose category from given list: ")
+            pprint(buyer.get_best_products(ctgr))
+
         elif command == 'best sellers':
             print(categories, '\n')
             ctgr = input("Choose category from given list: ")
             pprint(buyer.get_best_sellers(ctgr))
 
-        # elif command == 'worst sellers':
-        #     print(categories, '\n')
-        #     ctgr = input("Choose category from given list: ")
-        #     pprint(buyer.get_worst_sellers(ctgr))
-        #
-        #
-        # elif command == 'best shipping':
-        #     print("Print type of product")
-        #     print("F.e.: 'phone charger'")
-        #     product = input("Product: ")
-        #     pprint(buyer.find_best_shipping(product))
-        #
+        elif command == 'worst sellers':
+            print(categories, '\n')
+            ctgr = input("Choose category from given list: ")
+            pprint(buyer.get_worst_sellers(ctgr))
+
+        elif command == 'best shipping':
+            print("Print type of product")
+            print("F.e.: 'phone charger'")
+            product = input("Product: ")
+            pprint(buyer.find_best_shipping(product))
+
         elif command == 'alternative':
             print("Give me your product name")
             print("F.e.: 'phone charger'")

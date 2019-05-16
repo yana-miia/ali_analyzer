fbc55837750080f6f19c1c30790413cb40c6fe56
Yana Kryshchuk <kryshchuk@ucu.edu.ua>
2019-05-16 13:39:02 +0300

    added tests for Buyer

---
+++ tests.py
@@ -0,0 +1,56 @@
+from pprint import pprint
+from seller import Seller
+from buyer import Buyer
+
+#Uncomment the following blocks to test them
+if __name__ == '__main__':
+    seller = Seller("Solomiia")
+    print(seller)
+
+    # print("Best Selling:")
+    # for product in seller.get_best_products():
+    #    print(product)
+    # print("\n\n\n")
+    #
+    # print("Demand:")
+    # pprint(seller.find_demand())
+    # print("\n\n\n")
+    #
+    # print("Best Sellers:")
+    # pprint(seller.get_best_sellers())
+    # print("\n\n\n")
+    #
+    # print("Statistic about 'phone charger':")
+    # pprint(seller.get_product_statistic('phone charger'))
+    # print("\n\n\n")
+
+    # print("Analyzed:")
+    # for i in seller.get_personal_recommendations([32922653638, 32954832491]):
+    #    print(i)
+    # print("\n\n\n")
+
+# ----------------------------------------------------------------
+
+    # buyer = Buyer("Yana")
+    # print(buyer)
+    #
+    # print("Best products:")
+    # for product in buyer.get_best_products():
+    #     print(product)
+    # print("\n\n\n")
+
+    # print("Best sellers:")
+    # for seller in buyer.get_best_sellers():
+    #     print(seller)
+    # print("\n\n\n")
+    #
+    # print("Worst sellers:")
+    # for seller in buyer.get_worst_sellers():
+    #     print(seller)
+    # print("\n\n\n")
+    #
+    # print("Best shipping option:")
+    # print("\n\n\n")
+    #
+    # print("Alternatives for the product:")
+    # print("\n\n\n")
